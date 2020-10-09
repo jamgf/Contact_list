16469157d628c094f6f17556a7d82532ad917ea1
jamgf <46873184+jamgf@users.noreply.github.com>
2020-10-09 17:30:04 -0300

    Last version

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'components/homepage.dart';
+import 'components/contact_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
